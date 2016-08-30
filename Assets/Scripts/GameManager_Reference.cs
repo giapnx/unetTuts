4c0e1bd7f966f7c524ce67af717b06e383298916
GiapNX <giapnx@rikkeisoft.com>
2016-08-30 11:21:26 +0700

    update camLocalPosition

--- Assets/Scripts/GameManager_Reference.cs
+++ Assets/Scripts/GameManager_Reference.cs
@@ -8,12 +8,13 @@ public class GameManager_Reference : NetworkBehaviour {
 	[SerializeField] GameObject playerSpawn;
 	[SerializeField] GameObject joystick;
 	[SerializeField] Camera mainCamera;
+	private Vector3 camLocalPosition = new Vector3(0.02f, 0.8f, 0.15f);
 
 	public override void  OnStartServer()
 	{
 		GameObject go = GameObject.Instantiate (playerPrefab, playerSpawn.transform.position, Quaternion.identity) as GameObject;
 		mainCamera.transform.SetParent (go.transform);
-		mainCamera.transform.localPosition = new Vector3 (0, 0.8f, 0);
+		mainCamera.transform.localPosition = camLocalPosition;
 		NetworkServer.Spawn (go);
 	}
 
@@ -22,6 +23,9 @@ public class GameManager_Reference : NetworkBehaviour {
 		if (!isServer)
 		{
 			joystick.SetActive (false);
+			GameObject go = GameObject.FindWithTag ("Player");
+			mainCamera.transform.SetParent (go.transform);
+			mainCamera.transform.localPosition = camLocalPosition;
 		}
 	}
 }
