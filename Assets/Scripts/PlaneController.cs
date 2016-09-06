e4b5d0101b386fe13e5a7b659547b3ed36401269
Hungtm <manhhungvoi@gmail.com>
2016-09-06 17:41:20 +0700

    add animal

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlaneController : MonoBehaviour {
+
+    void Start () {
+
+        //iTween.MoveTo(gameObject, iTween.Hash("x", 3, "time", 4, "delay", 1, "onupdate", "myUpdateFunction", "looptype", iTween.LoopType.pingPong));
+        iTween.MoveTo(gameObject, gameObject.transform.forward,10);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+    
+    
+}
