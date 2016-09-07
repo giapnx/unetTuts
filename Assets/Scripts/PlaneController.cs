3db0a375517ff34ed5d90f810455c84f48a0978d
Hungtm <manhhungvoi@gmail.com>
2016-09-07 10:07:52 +0700

    add map

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class PlaneController : MonoBehaviour {
 
     void Start () {
-        iTween.MoveTo(gameObject, iTween.Hash("path",iTweenPath.GetPath("PlanePath"),"time",50f));
+        iTween.MoveTo(gameObject, iTween.Hash("path",iTweenPath.GetPath("PlanePath"),"time",30f,"looptype",iTween.LoopType.loop,"easetype",iTween.EaseType.linear));
         
     }
 	
---
+++ Assets/Scripts/RhinoController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RhinoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("RhinoPath"), "time", 30f, "easetype", iTween.EaseType.linear,"looptype",iTween.LoopType.loop));
+	}
+	
+	
+}
